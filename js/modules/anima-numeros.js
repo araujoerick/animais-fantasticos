56f8a4dd74520d314815c26d2764b8c7ba847653
Erick Araújo <araujoerick.dev@gmail.com>
2024-04-12 11:06:08 -0300

    :recycle: refactor: Transforma AnimaNumeros em Class

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,41 +1,69 @@
-export default function initAnimaNumeros() {
-  function animaNumeros() {
-    const numeros = document.querySelectorAll("[data-numero]");
+export default class AnimaNumeros {
+  constructor(numeros, observerTarget, observerClass) {
+    this.numeros = document.querySelectorAll(numeros);
+    this.observerTarget = document.querySelector(observerTarget); // Elemento a ser observado
+    this.observerClass = observerClass;
 
-    numeros.forEach((numero) => {
-      const total = +numero.innerText;
-      // Math.floor para arredondar o número
-      // Divide por cem o valor total e adicona la no incremento
-      // Para que a contagem não seja de um em um
-      const incremento = Math.floor(total / 100);
-      let start = 0;
+    // bind o this do objeto ao callback de handleMutation
+    this.handleMutation = this.handleMutation.bind(this);
+  }
 
-      const timer = setInterval(() => {
-        start += incremento;
-        numero.innerText = start;
-        // Se start for maior que total, limpa o intervalo
-        if (start > total) {
-          // Como o número passa e seta um valor errado, é interessante definir como o valor total no final
-          numero.innerText = total;
+  // Quando tem um método em uma classe que não precisa do objeto da class para funcionar
+  // Devemos definir como uma função estática. Então essa função agora faz parte da class
+  // AnimaNumeros e não do objeto que é criado a partir da AnimaNumeros
+  // Porém não é possível utlizar ela direto com o this agora,  então é necessário puxar ela
+  // do construtor dela
+  // Método que pega o número dentro de uma tag de elemento no DOM
+  static incrementarNumero(numero) {
+    const total = +numero.innerText;
+    // Math.floor para arredondar o número
+    // Divide por cem o valor total e adiciona la no incremento
+    // Para que a contagem não seja de um em um
+    const incremento = Math.floor(total / 100);
+    let start = 0;
 
-          clearInterval(timer);
-        }
-      }, 20 * Math.random());
-    });
+    const timer = setInterval(() => {
+      start += incremento;
+      numero.innerText = start;
+      // Se start for maior que total, limpa o intervalo
+      if (start > total) {
+        // Como o número passa e seta um valor a mais e errado, definimos o valor final no DOM como o próprio valor que foi retirado do DOM.
+        numero.innerText = total;
+        // E paramos o setInterval
+        clearInterval(timer);
+      }
+    }, 20 * Math.random()); // Random para o intervalo ficar diferente e mais dinâmico
+  }
+
+  animaNumeros() {
+    this.numeros.forEach(
+      (numero) => this.constructor.incrementarNumero(numero) // Puxa do construtor
+    );
   }
 
   // O mutation é um parâmetro tipo o event. Ele retorna uma array like com a mutação capturada
-  function handleMutation(mutation) {
-    if (mutation[0].target.classList.contains("ativo")) {
-      observer.disconnect();
-      animaNumeros();
+  // Então pegamos o target do primeiro item da array e verifica se contém a class "ativo"
+  // Se sim, para o observer e ativa a função de animar os números
+  handleMutation(mutation) {
+    if (mutation[0].target.classList.contains(this.observerClass)) {
+      this.observer.disconnect();
+      this.animaNumeros();
     }
   }
-  // Elemento a ser observado
-  const observerTarget = document.querySelector(".numeros");
 
-  // O observador observa mudanças de atributo do elemento definido, e então execulta a função de callback setada
-  const observer = new MutationObserver(handleMutation);
+  addMutatinObserver() {
+    // O observer é criado e adicionado como método da classe
+    // O this.handleMutation não está referenciando a classe e sim ao observer, então é
+    // necessário fazer o bind
+    this.observer = new MutationObserver(this.handleMutation);
+    // O observador observa mudanças de atributo do elemento definido, e então executa a função de callback setada. Nesse caso quando aparecer a classe "ativo" no DOM
+    this.observer.observe(this.observerTarget, { attributes: true });
+  }
 
-  observer.observe(observerTarget, { attributes: true });
+  init() {
+    if (this.numeros.length || this.observerTarget) {
+      this.addMutatinObserver();
+    }
+    return this;
+  }
 }
--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
 import initDropDownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-import initAnimaNumeros from "./modules/anima-numeros.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
@@ -35,7 +34,6 @@ tooltip.init();
 
 initDropDownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
