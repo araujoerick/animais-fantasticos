fc4c34ea531989b8ddd55bd18215f378a2700dde
Erick Araújo <araujoerick.dev@gmail.com>
2024-04-12 22:25:40 -0300

    :recycle: refactor: Transforma DropdownMenu em Class

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,30 +1,53 @@
 import outsideClick from "./outsideclick.js";
 
-export default function initDropDownMenu() {
-  const dropdowsMenus = document.querySelectorAll("[data-dropdown]");
+export default class DropdownMenu {
+  constructor(dropdownMenus, events) {
+    this.dropdowsMenus = document.querySelectorAll(dropdownMenus);
 
-  function handleClick(event) {
+    // O evento de clique também funciona no mobile, porém ele aguarda 300ms
+    // "click" Evento do mouse | "touchstart" Evento touch mobile
+    // Podemos adicionar vários eventos utilizando uma array
+    if (events === undefined) this.events = ["touchstart", "click"];
+    else this.events = events;
+
+    this.activeClass = "active";
+
+    // Bind no callback do evento
+    this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+  }
+
+  // Ativa o dropdown menu e adiciona a função que observa o clique fora
+  activeDropdownMenu(event) {
     event.preventDefault();
     // this se refere ao próprio menu que está sendo capturado através do evento click no loop do forEach
-    this.classList.add("active");
+    // O this agora seria o objeto da classe, então temos que mudar de this para
+    // event.currentTarget para poder capturar o elemento no DOM
+    const element = event.currentTarget;
+    element.classList.add(this.activeClass);
 
     // this é o element, array é o events e callback é afunção a ser ativada no fim da execução
-    outsideClick(this, ["click", "touchstart"], () => {
-      this.classList.remove("active");
+
+    outsideClick(element, this.events, () => {
+      element.classList.remove(this.activeClass);
     });
   }
 
-  dropdowsMenus.forEach((menu) => {
-    // O evento de clique também funciona no mobile, porém ele aguarda 300ms
-    // menu.addEventListener("click", handleClick); // Evento do mouse
-    // menu.addEventListener("touchstart", handleClick); // Evento touch mobile
-
-    // Podemos adicionar vários eventos utilizando uma array
-    ["click", "touchstart"].forEach((userEvent) => {
-      menu.addEventListener(userEvent, handleClick);
+  // Adiciona os eventos ao dropdown menu
+  addDropdownMenusEvent() {
+    this.dropdowsMenus.forEach((menu) => {
+      this.events.forEach((userEvent) => {
+        menu.addEventListener(userEvent, this.activeDropdownMenu);
+      });
     });
-  });
+  }
 
-  // AULA 0506 - EVENT BUBBLE
+  init() {
+    if (this.dropdowsMenus.length) {
+      this.addDropdownMenusEvent();
+    }
+    return this;
+  }
+
+  // EVENT BUBBLE
   // O JS ao terminar de executar uma função com evento, verifica se tem outros eventos nos pais e se tiver, executa-os
 }
