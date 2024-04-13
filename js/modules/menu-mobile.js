721f8c69eb19ddde52c5a6e5c59e638eabb97f93
Erick Araújo <araujoerick.dev@gmail.com>
2024-04-12 23:30:07 -0300

    :recycle: refactor: Transforma MenuMobile em Class

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,23 +1,41 @@
 import outsideClick from "./outsideclick.js";
 
-export default function initMenuMobile() {
-  const menuButton = document.querySelector("[data-menu='button']");
-  const menuList = document.querySelector("[data-menu='list']");
-  const eventos = ["click", "touchstart"];
+export default class MenuMobile {
+  constructor(menuButton, menuList, events) {
+    this.menuButton = document.querySelector(menuButton);
+    this.menuList = document.querySelector(menuList);
+    // O evento de clique também funciona no mobile, porém ele aguarda 300ms
+    // "click" Evento do mouse | "touchstart" Evento touch mobile
+    // Podemos adicionar vários eventos utilizando uma array
+    if (events === undefined) this.events = ["touchstart", "click"];
+    else this.events = events;
+    this.activeClass = "active";
 
-  function openMenu() {
-    menuButton.classList.add("active");
-    menuList.classList.add("active");
+    this.openMenu = this.openMenu.bind(this);
+  }
+
+  openMenu() {
+    this.menuButton.classList.add(this.activeClass);
+    this.menuList.classList.add(this.activeClass);
     // Função importada, recebe o elemento que não deve ser clicado para ativar a função | Recebe o evento, nesse caso uma array | E a função de callback que nesse caso é anônima para remover as classes
     // Para funcionar é necessário adicionar o setTimeOut lá no outSideClick para que o callback só seja ativado após a fase de bubble (que ativa todos os eventos pais)
-    outsideClick(menuList, eventos, () => {
-      menuButton.classList.remove("active");
-      menuList.classList.remove("active");
+    outsideClick(this.menuList, this.events, () => {
+      this.menuButton.classList.remove(this.activeClass);
+      this.menuList.classList.remove(this.activeClass);
     });
   }
 
-  if (menuButton) {
+  addMenuMobileEvents() {
     // Como os eventos estão em uma array, é preciso de um forEach para selecionar um dos eventos que o usuário startar seja no click ou touch do celular
-    eventos.forEach((evento) => menuButton.addEventListener(evento, openMenu));
+    this.events.forEach((evento) =>
+      this.menuButton.addEventListener(evento, this.openMenu)
+    );
+  }
+
+  init() {
+    if (this.menuButton && this.menuList) {
+      this.addMenuMobileEvents();
+    }
+    return this;
   }
 }
